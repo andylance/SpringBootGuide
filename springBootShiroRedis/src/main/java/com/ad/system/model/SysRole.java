6225a496036ba5346c4f3e18b9be5fd7a4e388e1
andylance <andylance@123.com>
2021-03-01 14:42:16 +0800

    fix: 修复bug, 更新代码

--- springBootShiroRedis/src/main/java/com/ad/system/model/SysRole.java
+++ springBootShiroRedis/src/main/java/com/ad/system/model/SysRole.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
  * 系统用户角色
  */
 public class SysRole implements Serializable {
-    private static final long serialVersionUID = 1383391516427315432L;
+    private static final long serialVersionUID = 2543291254171962178L;
     /**
      * 角色ID
      */
