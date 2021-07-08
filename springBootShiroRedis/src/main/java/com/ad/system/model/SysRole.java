f25964587a90430870f7554b1637a01df205b9a7
andylance <andylance@123.com>
2021-07-08 21:50:23 +0800

    refactor: 逐渐切换 lombok

--- springBootShiroRedis/src/main/java/com/ad/system/model/SysRole.java
+++ springBootShiroRedis/src/main/java/com/ad/system/model/SysRole.java
@@ -1,10 +1,13 @@
 package com.ad.system.model;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
 /**
  * 系统用户角色
  */
+@Data
 public class SysRole implements Serializable {
     private static final long serialVersionUID = 2543291254171962178L;
     /**
@@ -17,23 +20,7 @@ public class SysRole implements Serializable {
      */
     private String roleDesc;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getRoleDesc() {
-        return roleDesc;
-    }
-
-    public void setRoleDesc(String roleDesc) {
-        this.roleDesc = roleDesc == null ? null : roleDesc.trim();
-    }
-
-    @java.lang.Override
+    @Override
     public java.lang.String toString() {
         return "SysRole{" +
                 "id=" + id +
