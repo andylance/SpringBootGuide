7629fcc40bb146b73e4ef2b6541114603c799a43
andylance <andylance@123.com>
2021-10-08 11:21:30 +0800

    refactor: 逐渐切换 lombok 2021.10.08

--- springBootShiroRedis/src/main/java/com/ad/system/model/SysRoleResources.java
+++ springBootShiroRedis/src/main/java/com/ad/system/model/SysRoleResources.java
@@ -1,10 +1,14 @@
 package com.ad.system.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SysRoleResources implements Serializable {
     private static final long serialVersionUID = 6265196654259238748L;
     private Integer id;
